7649c388f62f4447970bd78a27f6780b5c82190c
cerberodev <hi@cerbero.dev>
2021-11-03 20:52:31 -0500

    partial changes

--- lib/ui/list_user.dart
+++ lib/ui/list_user.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:userapp/models/user.dart';
 import 'package:userapp/repository/api.dart';
+import 'package:userapp/ui/ui.dart';
 import 'package:userapp/ui/widgets/widgets.dart';
 
 class ListUser extends StatelessWidget {
@@ -13,15 +14,25 @@ class ListUser extends StatelessWidget {
         builder: (context, AsyncSnapshot<List<User>> snapshot) {
           if (snapshot.hasData) {
             return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return UserWidget(
-                    imageUrl: snapshot.data![index].avatar_url,
-                    subtitle: '',
-                    title: '',
-                    trailing: 4,
-                  );
-                });
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return UserWidget(
+                  imageUrl: snapshot.data![index].avatar_url,
+                  subtitle: snapshot.data![index].html_url,
+                  title: snapshot.data![index].login,
+                  trailing: snapshot.data![index].id,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<UserDetailPage>(
+                        builder: (context) =>
+                            UserDetailPage(user: snapshot.data![index]),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
--- lib/ui/user_detail.dart
+++ lib/ui/user_detail.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:userapp/models/user.dart';
 
@@ -14,20 +16,60 @@ class UserDetailPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Center(
-          child: Card(
-              color: Colors.teal,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                //  TODO: Modularize this widget
+                color: Colors.teal,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                elevation: 5,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Image.network(
+                        user.avatar_url,
+                        fit: BoxFit.cover,
+                        width: 400,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        'This username is: ${user.login}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              elevation: 5,
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(25),
-                    child: Image.network(user.avatar_url),
+              const SizedBox(height: 10),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 5,
+                color: Colors.purple[300],
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  child: Text(
+                    'ID this user is: ${user.id}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ],
-              )),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
